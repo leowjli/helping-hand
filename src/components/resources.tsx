a3d6e62be22587590141a8c84f8397ee173b5588
leowjli <leo.wj.li@gmail.com>
2024-10-13 10:27:39 -0700

    added map link to partyrock

--- src/components/resources.tsx
+++ src/components/resources.tsx
@@ -26,12 +26,12 @@ const Resources = React.forwardRef<HTMLElement>((props, ref) => {
         // Resources
         <section ref={ref}>
             <div className="row">
-                <ResCard link={"*"} img={"../../images/food.png"} alt={"food icon"} title={"Food"} />
-                <ResCard link={"*"} img={"../../images/Shelter.png"} alt={"shelter icon"} title={"Shelter"} />
-                <ResCard link={"*"} img={"../../images/water.png"} alt={"bottled water icon"} title={"Water"} />
-                <ResCard link={"*"} img={"../../images/medicine.png"} alt={"Red Cross icon"} title={"Medical Care"} />
-                <ResCard link={"*"} img={"../../images/other_support.png"} alt={"box with hearts icon"} title={"Volunteer & Donate"} />
-                <ResCard link={"*"} img={"../../images/clothing.png"} alt={"additional resources pants icon"} title={"Additional Resources"} />
+                <ResCard link={"https://partyrock.aws/u/jgashler/6ojuTDRiH/Community-Care-Map"} img={"../../images/food.png"} alt={"food icon"} title={"Food"} />
+                <ResCard link={"https://partyrock.aws/u/jgashler/6ojuTDRiH/Community-Care-Map"} img={"../../images/Shelter.png"} alt={"shelter icon"} title={"Shelter"} />
+                <ResCard link={"https://partyrock.aws/u/jgashler/6ojuTDRiH/Community-Care-Map"} img={"../../images/water.png"} alt={"bottled water icon"} title={"Water"} />
+                <ResCard link={"https://partyrock.aws/u/jgashler/6ojuTDRiH/Community-Care-Map"} img={"../../images/medicine.png"} alt={"Red Cross icon"} title={"Medical Care"} />
+                <ResCard link={"https://partyrock.aws/u/jgashler/6ojuTDRiH/Community-Care-Map"} img={"../../images/other_support.png"} alt={"box with hearts icon"} title={"Volunteer & Donate"} />
+                <ResCard link={"https://partyrock.aws/u/jgashler/6ojuTDRiH/Community-Care-Map"} img={"../../images/clothing.png"} alt={"additional resources pants icon"} title={"Additional Resources"} />
             </div>
         </section>
     );
